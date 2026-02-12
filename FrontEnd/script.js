ede47d836f96ccf84ac9ac439b029f589b361ed3
hugo ferey <ferey.hugo@gmail.com>
2026-02-12 21:57:59 +0100

    feat : modification du script

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,1 +1,26 @@
 console.log("Hello World!");
+
+let works =[];
+
+async function afficherWorks() {
+  const reponse = await fetch("http://localhost:5678/api/works");
+    works = await reponse.json();
+  console.log(works);
+}
+
+afficherWorks();
+
+const doubles = works.map(work => {
+    console.log("test");
+  return work.id;
+});
+console.log(doubles);
+
+const nombres = [1, 2, 3, 4, 5];
+
+const double = nombres.map(nombre => {
+  return nombre * 2;
+});
+
+console.log(double);
+

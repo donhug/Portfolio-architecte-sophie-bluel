5d410023295c3de4ead2c2545b3218b999c5f50c
hugo ferey <ferey.hugo@gmail.com>
2026-02-22 01:11:24 +0100

    feat : ajout du tri(première version)

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,13 +2,14 @@ console.log("Hello World!");
 
 let works =[];
 
+//récupere les images + titres
 async function afficherWorks() {
   const reponse = await fetch("http://localhost:5678/api/works");
   works = await reponse.json();
   console.log(works);
   genererGalerie(works);
 
-
+//récupere les 3 catégories
   let categoriesArray = new Set();
   works.forEach(works => {
     if (works.category){
@@ -19,9 +20,12 @@ async function afficherWorks() {
   console.log(categories);
 
   genererBoutons(categories)
+
+
 }
 
 afficherWorks();
+
 
 function genererGalerie(works){
   const divImages = document.querySelector(".gallery");
@@ -47,18 +51,30 @@ function genererGalerie(works){
 function genererBoutons(categories){
   const divButtons = document.querySelector(".btn");
 
+  const buttonAll = document.createElement("button");
+  buttonAll.className = "btnCategories";
+  buttonAll.innerText = "Tous";
+  divButtons.appendChild(buttonAll);
+  buttonAll.addEventListener("click", function(){
+    console.log("Tous");
+  })
+
   for(let i = 0; i < categories.length; i++ ){
-    const button = categories[i];
 
-    const buttonEl = document.createElement("button");
-    buttonEl.className = "btn";
-    //buttonEl.setAttribute("type", "button");
-    buttonEl.innerText = categories[i].name;
+    const buttonCat = document.createElement("button");
+    buttonCat.className = "btnCategories";
+    buttonCat.innerText = categories[i].name;
 
-    divButtons.appendChild(buttonEl);
+    divButtons.appendChild(buttonCat);
 
+    boutonTrie(buttonCat,categories[i], works);
   }
 }
 
-
-
+function boutonTrie(button,category, works){
+  button.addEventListener("click", function(){
+    console.log("ID => "+category.id+" /nom =>"+category.name );
+    const worksTrie = works.filter(works => works.categoryId === category.id);
+    worksTrie.forEach(works => console.log("(TITRE) = "+works.title + " (catégorie) = "+works.categoryId));
+  })
+}
